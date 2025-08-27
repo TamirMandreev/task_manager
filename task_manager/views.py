c5810c38e7505d4873e412b065c8ff05527b2a97
Tamir Mandreev <tamirmandreev@mail.ru>
2025-08-28 02:12:41 +0800

    Описал CRUD для модели Task

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+
+from models import Task
+from serializers import TaskSerializer
+
+
+class TaskCreateAPIView(CreateAPIView):
+    serializer_class = TaskSerializer
+
+
+class TaskListAPIView(ListAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+class TaskDetailAPIView(RetrieveAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TaskUpdateAPIView(UpdateAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TaskDeleteAPIView(DestroyAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
