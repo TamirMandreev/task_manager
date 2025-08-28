0c5eb5984cac1c2edf2eb91235cc692486cc6dbd
Tamir Mandreev <tamirmandreev@mail.ru>
2025-08-29 02:16:30 +0800

    Исправил динамическую часть маршрутов task-detail, task-update, task-delete

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 
-import views
+from task_manager import views
 
 app_name = 'task_manager'
 
 urlpatterns = [
     path('create/', views.TaskCreateAPIView.as_view(), name='task-create'),
     path('list/', views.TaskListAPIView.as_view(), name='task-list' ),
-    path('<int:pk>/', views.TaskDetailAPIView.as_view(), name='task-detail'),
-    path('update/<int:pk>/', views.TaskUpdateAPIView.as_view(), name='task-update'),
-    path('delete/<int:pk>/', views.TaskDeleteAPIView.as_view(), name='task-delete'),
+    path('<uuid:pk>/', views.TaskDetailAPIView.as_view(), name='task-detail'),
+    path('update/<uuid:pk>/', views.TaskUpdateAPIView.as_view(), name='task-update'),
+    path('delete/<uuid:pk>/', views.TaskDeleteAPIView.as_view(), name='task-delete'),
 ]
